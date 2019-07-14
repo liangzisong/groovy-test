205da6cc48aa48fbf698901055b428e4bee7bd67
liangzisong <18462146261@163.com>
2019-07-14 23:44:22 +0800

    first commit

---
+++ src/main/java/com/imooc/gradle/todo/App.java
@@ -0,0 +1,71 @@
+package com.imooc.gradle.todo;//
+//
+//
+//
+
+////////////////////////////////////////////////////////////////////
+//                          _ooOoo_                               //
+//                         o8888888o                              //
+//                         88" . "88                              //
+//                         (| ^_^ |)                              //
+//                         O\  =  /O                              //
+//                      ____/`---'\____                           //
+//                    .'  \\|     |//  `.                         //
+//                   /  \\|||  :  |||//  \                        //
+//                  /  _||||| -:- |||||-  \                       //
+//                  |   | \\\  -  /// |   |                       //
+//                  | \_|  ''\---/''  |   |                       //
+//                  \  .-\__  `-`  ___/-. /                       //
+//                ___`. .'  /--.--\  `. . ___                     //
+//              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+//            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+//            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+//      ========`-.____`-.___\_____/___.-`____.-'========         //
+//                           `=---='                              //
+//      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+//         佛祖保佑       永无BUG     永不修改                       //
+////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+//让需求简单一点，心灵就会更轻松一点；
+//让外表简单一点，本色就会更接近一点；
+//让过程简单一点，内涵就会更丰富一点；
+//
+//
+//
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+/**
+ * Copyright (C), 2002-2019
+ * FileName: App
+ * <p>
+ * Description:
+ *
+ * @author 如果这段代码非常棒就是梁子松写的
+ * 如果这代码挺差劲那么我也不知道是谁写的
+ * @version 1.0.0
+ * @create 19-7-14 下午8:55
+ */
+public class App {
+
+    static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (++i>0){
+            logger.info(i + ".please input todo item name :");
+            TodoItem todoItem = new TodoItem(scanner.nextLine());
+            logger.info("todoItem = " + todoItem);
+        }
+    }
+
+}
